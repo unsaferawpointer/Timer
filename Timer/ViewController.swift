ed8759b65eed1e4402558177809436be4870c991
Anton Cherkasov <unsaferawpointer@gmail.com>
2024-02-20 20:09:48 +0300

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Anton Cherkasov on 20.02.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
